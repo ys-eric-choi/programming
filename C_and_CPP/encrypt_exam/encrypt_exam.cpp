e21864be7c68fa851acf003ee8ba28764186d36c
Choi Yunsu <cyunsu77@naver.com>
2020-05-19 16:42:29 +0900

    add comment

--- C_and_CPP/encrypt_exam/encrypt_exam.cpp
+++ C_and_CPP/encrypt_exam/encrypt_exam.cpp
@@ -1,3 +1,5 @@
+// 2020.05.19
+// XOR 연산을 이용한 암호화/복호화 예시
 #include <iostream>
 
 #define FREE(X) { free(X); X = NULL; }
@@ -10,18 +12,21 @@ int main(void) {
 
     std::string strInput = "안녕하세요. ys-eric-choi 계정 입니다.";
 
+    // 암호화 Key
     const char* strKey = "@ys-eric-choi";
     int iKeyLen = strlen(strKey);
 
     int iLen = strInput.length();
 
+    // 실제 출력 문장 길이를 먼저 출력
     fwrite(&iLen, sizeof(int), 1, fp);
 
     char* strOut = (char*)strInput.c_str();
     char c;
 
     for(int i = 0; i < iLen; i++) {
-        c = strOut[i] ^ strKey[i%iKeyLen];
+        // XOR 연산으로 암호화
+        c = strOut[i] ^ strKey[i % iKeyLen];
         fwrite(&c, sizeof(char), 1, fp);
     }
 
@@ -30,13 +35,16 @@ int main(void) {
 
     fp = fopen("test.bin", "rb");
 
+    // 읽어야 하는 문장의 길이를 먼저 구함
     fread(&iLen, sizeof(int), 1, fp);
 
-    char* buf = (char*) malloc(sizeof(char) * iLen + 1);
+    // NULL문자('\0') 삽입을 위해 길이보다 +1 만큼 메모리를 할당한다.
+    char* buf = (char*) malloc(sizeof(char) * (iLen + 1));
 
     for(int i = 0; i < iLen; i++) {
         fread(&c, sizeof(char), 1, fp);
-        buf[i] = c ^ strKey[i%iKeyLen];
+        // XOR 연산으로 복호화
+        buf[i] = c ^ strKey[i % iKeyLen];
     }
 
     buf[iLen] = '\0';
@@ -44,6 +52,7 @@ int main(void) {
 
     fclose(fp);
 
+    // 동적할당 메모리 해제
     FREE(buf);
 
     return 0;
